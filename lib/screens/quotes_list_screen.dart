57b56c583164f1fbcb0db96bddc2f518681a9113
tataadin <tataa2237@gmail.com>
2024-02-08 13:04:43 +0700

    add quote list module

---
+++ lib/screens/quotes_list_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../models/quote.dart';
+
+class QuotesListScreen extends StatelessWidget {
+  final List<Quote> quotes;
+
+  const QuotesListScreen({
+    Key? key,
+    required this.quotes,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Quotes App"),
+      ),
+      body: ListView(
+        children: [
+          for (var quote in quotes)
+            ListTile(
+              title: Text(quote.author),
+              subtitle: Text(quote.quote),
+              isThreeLine: true,
+            )
+        ],
+      ),
+    );
+  }
+}
+class QuoteDetailsScreen extends StatelessWidget {
+  final String quoteId;
+
+  const QuoteDetailsScreen({
+    Key? key,
+    required this.quoteId,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final quote = quotes.singleWhere((element) => element.id == quoteId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(quote.author),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(quote.author, style: Theme.of(context).textTheme.headline6),
+            Text(quote.quote, style: Theme.of(context).textTheme.subtitle1),
+          ],
+        ),
+      ),
+    );
+  }
+}
