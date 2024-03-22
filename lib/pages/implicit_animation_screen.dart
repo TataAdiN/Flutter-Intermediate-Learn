2599bedca0576ee882e0ce6d58aea36a792b858f
tataadin <tataa2237@gmail.com>
2024-03-22 22:22:27 +0700

    add animation when adding new item

--- lib/pages/implicit_animation_screen.dart
+++ lib/pages/implicit_animation_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'item_card.dart';
@@ -6,6 +8,7 @@ class ImplicitAnimationScreen extends StatelessWidget {
   ImplicitAnimationScreen({Key? key}) : super(key: key);
 
   final List<int> items = [26, 45];
+  final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +17,23 @@ class ImplicitAnimationScreen extends StatelessWidget {
         title: const Text("Animated List Project"),
       ),
       body: AnimatedList(
+        key: listKey,
         initialItemCount: items.length,
         itemBuilder: (context, index, animation) {
           return ItemCard(
             item: items[index],
+            animation: animation,
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final randNumber = Random().nextInt(100);
+          const index = 0;
+          items.insert(index, randNumber);
+          listKey.currentState?.insertItem(index);
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
--- lib/pages/item_card.dart
+++ lib/pages/item_card.dart
@@ -1,34 +1,41 @@
 import 'package:flutter/material.dart';
 
 class ItemCard extends StatelessWidget {
-  const ItemCard({super.key, required this.item});
+  const ItemCard({super.key, required this.item, required this.animation});
 
   final int item;
+  final Animation<double> animation;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.primaries[item % Colors.primaries.length],
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "Number $item",
-              style: const TextStyle(
-                fontSize: 24,
-                color: Colors.white,
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(-1, 0),
+        end: const Offset(0, 0),
+      ).animate(animation),
+      child: Card(
+        color: Colors.primaries[item % Colors.primaries.length],
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Number $item",
+                style: const TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.white,
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
