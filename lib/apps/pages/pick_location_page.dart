f683d2044c9113f2294c6dab9fb1bcc7d7c17dd0
tataadin <tataa2237@gmail.com>
2024-03-30 16:26:44 +0700

    implement module with bloc pick location [b29b2a0405234c0b38dd2c2dd1f44395ba23f81c]

--- lib/apps/pages/pick_location_page.dart
+++ lib/apps/pages/pick_location_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../l10n/localizations.dart';
 import '../../views/pick_location_view.dart';
+import '../blocs/pick_location_bloc.dart';
 
 class PickLocationPage extends StatelessWidget {
   const PickLocationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const PickLocationView();
+    AppLocalizations localizations = AppLocalizations.of(context)!;
+    return BlocProvider(
+      create: (BuildContext context) => PickLocationBloc(localization: localizations),
+      child: const PickLocationView(),
+    );
   }
 }
--- lib/views/pick_location_view.dart
+++ lib/views/pick_location_view.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart' as geocoding;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_intermediate_learn/apps/states/pick_location/pick_location_state_new_latlng.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart' as geocoding;
 
+import '../apps/blocs/pick_location_bloc.dart';
 import '../apps/data/enums/app_button_align.dart';
+import '../apps/events/pick_location/pick_location_event_longpress.dart';
+import '../apps/states/pick_location/pick_location_state.dart';
 import '../utils/responsive_screen.dart';
 import '../widgets/components/app_button.dart';
 import 'widgets/maps/placemark_card.dart';
@@ -15,8 +20,8 @@ class PickLocationView extends StatefulWidget {
 }
 
 class _PickLocationViewState extends State<PickLocationView> {
-  geocoding.Placemark? placemark;
   final dicodingOffice = const LatLng(-6.8957473, 107.6337669);
+  late GoogleMapController mapController;
 
   @override
   Widget build(BuildContext context) {
@@ -24,56 +29,81 @@ class _PickLocationViewState extends State<PickLocationView> {
       appBar: AppBar(
         title: const Text('Pick Location'),
       ),
-      body: Center(
-        child: Stack(
-          children: [
-            GoogleMap(
-              myLocationEnabled: true,
-              initialCameraPosition: CameraPosition(
-                zoom: 18,
-                target: dicodingOffice,
-              ),
-              zoomControlsEnabled: false,
-              mapToolbarEnabled: false,
-              myLocationButtonEnabled: false,
-              onMapCreated: (controller) async {
-              },
-              onLongPress: (LatLng latLng) {
-              },
+      body: BlocConsumer<PickLocationBloc, PickLocationState>(
+        builder: (context, state) {
+          Set<Marker> marker = {};
+          geocoding.Placemark? placemark;
+          if (state is PickLocationStateNewLatLng) {
+            placemark = state.placeMark;
+            marker = state.marker;
+            mapController
+                .animateCamera(CameraUpdate.newLatLng(state.newLatLng));
+          }
+          return mapView(marker: marker, placemark: placemark);
+        },
+        listener: (BuildContext context, PickLocationState state) {},
+      ),
+    );
+  }
+
+  Center mapView(
+      {required Set<Marker> marker, geocoding.Placemark? placemark}) {
+    return Center(
+      child: Stack(
+        children: [
+          GoogleMap(
+            markers: marker,
+            myLocationEnabled: true,
+            initialCameraPosition: CameraPosition(
+              zoom: 18,
+              target: dicodingOffice,
             ),
-            Positioned(
-              top: 16,
-              right: 16,
-              child: FloatingActionButton(
-                child: const Icon(Icons.my_location),
-                onPressed: () {},
-              ),
+            zoomControlsEnabled: false,
+            mapToolbarEnabled: false,
+            myLocationButtonEnabled: false,
+            onMapCreated: (controller) async {
+              mapController = controller;
+            },
+            onLongPress: (LatLng latLng) {
+              context
+                  .read<PickLocationBloc>()
+                  .add(PickLocationEventLongPress(latLng: latLng));
+            },
+          ),
+          Positioned(
+            top: 16,
+            right: 16,
+            child: FloatingActionButton(
+              child: const Icon(Icons.my_location),
+              onPressed: () {},
             ),
+          ),
+          Positioned(
+            bottom: placemark != null
+                ? ResponsiveSize.fromWith(context, percentage: 24)
+                : 16,
+            right: 16,
+            child: AppButton(
+              height: ResponsiveSize.fromWith(context, percentage: 12),
+              align: AppButtonAlign.center,
+              onClick: () {},
+              label: 'Pick this Location',
+              icon: Icons.edit_location_alt,
+              color: Colors.green,
+            ),
+          ),
+          if (placemark == null)
+            const SizedBox()
+          else
             Positioned(
               bottom: 16,
               right: 16,
-              child: AppButton(
-                height: ResponsiveSize.fromWith(context, percentage: 12),
-                align: AppButtonAlign.center,
-                onClick: () {},
-                label: 'Pick this Location',
-                icon: Icons.edit_location_alt,
-                color: Colors.green,
+              left: 16,
+              child: PlacemarkCard(
+                placemark: placemark,
               ),
             ),
-            if (placemark == null)
-              const SizedBox()
-            else
-              Positioned(
-                bottom: 16,
-                right: 16,
-                left: 16,
-                child: PlacemarkCard(
-                  placemark: placemark!,
-                ),
-              ),
-          ],
-        ),
+        ],
       ),
     );
   }
